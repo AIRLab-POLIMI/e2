45d580d26cf541ebba91eec156ff6aac065df35c
boottp <sr.jackal@gmail.com>
2014-01-13 23:37:57 +0100

    Simulation code

---
+++ e2_simulator/src/base_controller.cpp
@@ -0,0 +1,156 @@
+/*************************************************************
+ *  Basic differential controller used by vrep simulator
+ *
+ *  by Lorenzo Ripani - AIRLab (Politecnico di Milano)
+ *  email: ripani.lorenzo@gmail.com
+ *
+ *  version 0.1 - 12/2014
+ *************************************************************/
+
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+
+// Data structures:
+#include "vrep_common/VrepInfo.h"
+#include "vrep_common/v_repConst.h"
+#include "vrep_common/JointSetStateData.h"
+
+// API services:
+#include "vrep_common/simRosEnablePublisher.h"
+#include "vrep_common/simRosEnableSubscriber.h"
+
+#define RATE 5000
+#define ROBOT_WIDTH 0.6
+
+// Global variables (modified by topic subscribers):
+bool simulationRunning=true;
+float simulationTime=0.0f;
+
+int leftMotorHandle,rightMotorHandle,neckMotorHandle;
+float LeftMotorSpeed,RightMotorSpeed,neckMotorSpeed;
+ros::Publisher motorSpeedPub;
+vrep_common::JointSetStateData motorSpeeds,neckSpeed;
+
+
+/*
+ * Send messages to vrep simulator, for neck rotation
+ */
+void neckCallback(const geometry_msgs::Twist::ConstPtr& msg){
+	ROS_INFO("Received neck velocity command:%f", msg->angular.z);
+
+	neckMotorSpeed = msg->angular.z * 5;
+
+	neckSpeed.handles.data.push_back(neckMotorHandle);
+	neckSpeed.setModes.data.push_back(2);
+
+	neckSpeed.values.data.push_back(neckMotorSpeed);
+	motorSpeedPub.publish(neckSpeed);
+}
+
+/*
+ * Send messages to vrep simulator in order to move robot base
+ */
+void controllerCallback(const geometry_msgs::Twist::ConstPtr& msg){
+
+	ROS_DEBUG("Received Twist Message");
+
+	LeftMotorSpeed=0;
+	RightMotorSpeed=0;
+
+	// Control loop e send back to simulator
+	LeftMotorSpeed = msg->linear.x - msg->angular.z * (ROBOT_WIDTH/2);
+	RightMotorSpeed = msg->linear.x + msg->angular.z * (ROBOT_WIDTH/2);
+
+	LeftMotorSpeed *=20;
+	RightMotorSpeed *=20;
+
+	motorSpeeds.handles.data.push_back(leftMotorHandle);
+	motorSpeeds.handles.data.push_back(rightMotorHandle);
+	motorSpeeds.setModes.data.push_back(2); // 2 is the speed mode
+	motorSpeeds.setModes.data.push_back(2);
+	motorSpeeds.values.data.push_back(LeftMotorSpeed);
+	motorSpeeds.values.data.push_back(RightMotorSpeed);
+	motorSpeedPub.publish(motorSpeeds);
+
+}
+
+// Topic subscriber callbacks:
+void infoCallback(const vrep_common::VrepInfo::ConstPtr& info){
+	simulationTime=info->simulationTime.data;
+	simulationRunning=(info->simulatorState.data&1)!=0;
+}
+
+
+//===================================================
+//	Main Code
+//===================================================
+int main(int argc,char* argv[]){
+
+	if (argc>=3){
+		leftMotorHandle=atoi(argv[1]);
+		rightMotorHandle=atoi(argv[2]);
+		neckMotorHandle=atoi(argv[3]);
+	}else{
+		printf("Indicate following arguments: 'leftMotorHandle rightMotorHandle NeckMotor'!\n");
+		sleep(5000);
+		return 0;
+	}
+
+	// Create a ROS node :
+	int _argc = 0;
+	char** _argv = NULL;
+	struct timeval tv;
+	unsigned int timeVal=0;
+
+	if (gettimeofday(&tv,NULL)==0)
+		timeVal=(tv.tv_sec*1000+tv.tv_usec/1000)&0x00ffffff;
+
+	std::string nodeName("e2_base_controller");
+	ros::init(_argc,_argv,nodeName);
+
+	if(!ros::master::check())
+		return(0);
+
+
+	ros::NodeHandle node("~");
+	printf("E2 Base Controller started.");
+
+
+	ros::Subscriber subInfo=node.subscribe("/vrep/info",1,infoCallback);
+
+	//  Let's tell V-REP to subscribe to the motor speed topic
+	ros::ServiceClient client_enableSubscriber=node.serviceClient<vrep_common::simRosEnableSubscriber>("/vrep/simRosEnableSubscriber");
+	vrep_common::simRosEnableSubscriber srv_enableSubscriber,srv_enableSubscriber_neck;
+
+	srv_enableSubscriber.request.topicName="/"+nodeName+"/joints"; // the topic name
+	srv_enableSubscriber.request.queueSize=1; // the subscriber queue size (on V-REP side)
+	srv_enableSubscriber.request.streamCmd=simros_strmcmd_set_joint_state; // the subscriber type
+
+
+	if ( client_enableSubscriber.call(srv_enableSubscriber)&&(srv_enableSubscriber.response.subscriberID!=-1) )
+	{
+		// Prepare a publisher of those motor speeds:
+		motorSpeedPub=node.advertise<vrep_common::JointSetStateData>("joints",1);
+
+		ros::Subscriber twist_sub = node.subscribe("/cmd_vel", RATE, controllerCallback);
+		ros::Subscriber neck_twist = node.subscribe("/e2/neck", RATE, neckCallback);
+
+		while (ros::ok()&&simulationRunning)
+		{
+
+			// handle ROS messages:
+			ros::spinOnce();
+
+			// sleep a bit:
+			usleep(RATE);
+		}
+
+	}
+
+
+
+}
+
+
+
+
---
+++ e2_simulator/src/e2_fake_odom.cpp
@@ -0,0 +1,112 @@
+/*************************************************************
+ * Get messages from vrep and generate fake odom messages,
+ * used to correctly simulate robot navigation system.
+ *
+ * by Lorenzo Ripani - AIRLab (Politecnico di Milano)
+ * email: ripani.lorenzo@gmail.com
+ *
+ * version 0.1 -  01/2014
+ *************************************************************/
+
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <cmath>
+
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+
+#define RATE	5000
+
+geometry_msgs::PoseStamped e2_pose;
+geometry_msgs::TwistStamped e2_twist;
+
+/**
+ * Get pose messages from vrep.
+ */
+void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	e2_pose.pose=msg->pose;
+	ROS_DEBUG("Pose x,y,z: [%f,%f,%f]", msg->pose.position.x,msg->pose.position.y,msg->pose.position.z);
+	ROS_DEBUG("E2_Pose x,y,z: [%f,%f,%f]", e2_pose.pose.position.x,e2_pose.pose.position.y,e2_pose.pose.position.z);
+}
+
+/*
+ * Get orientation messages from vrep
+ */
+void twistCallback(const geometry_msgs::TwistStamped::ConstPtr& msg){
+	ROS_DEBUG("Twist-linear: [%f,%f,%f]", msg->twist.linear.x, msg->twist.linear.y, msg->twist.linear.z);
+	e2_twist.twist=msg->twist;
+}
+
+//===================================================
+// Main Code
+//===================================================
+int main(int argc, char** argv){
+
+	ros::init(argc, argv, "odometry_publisher");
+
+	ros::NodeHandle n;
+	ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+	ros::Subscriber pose_sub = n.subscribe("/e2/pose", 1000, poseCallback);
+	ros::Subscriber twist_sub = n.subscribe("/e2/twist", 1000, twistCallback);
+
+	tf::TransformBroadcaster odom_broadcaster;
+
+	ros::Time current_time, last_time;
+	last_time = ros::Time::now();
+	current_time = ros::Time::now();
+	ros::Rate r(5.0);
+
+	ROS_INFO("Fake odom Up and Running......");
+
+	while(n.ok()){
+
+		 // check for incoming messages
+		ros::spinOnce();
+
+		current_time = ros::Time::now();
+
+		//all odometry is 6DOF use quaternion
+		//geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(e2_pose.pose.);
+		double yaw = asin(1);
+		geometry_msgs::Quaternion odom_quat = e2_pose.pose.orientation;
+
+		geometry_msgs::TransformStamped odom_trans;
+		odom_trans.header.stamp = current_time;
+		odom_trans.header.frame_id = "odom";
+		odom_trans.child_frame_id = "base_footprint";
+
+		// Add position
+		odom_trans.transform.translation.x = e2_pose.pose.position.x;
+		odom_trans.transform.translation.y = e2_pose.pose.position.y;
+		odom_trans.transform.translation.z = 0.0;
+		// Add orientation
+		odom_trans.transform.rotation = odom_quat;
+
+		// Send the transform
+		odom_broadcaster.sendTransform(odom_trans);
+
+		// Publishing odometry message over ROS
+		nav_msgs::Odometry odom;
+		odom.header.stamp = current_time;
+		odom.header.frame_id = "odom";
+
+		// Set the position
+		odom.pose.pose= e2_pose.pose;
+		odom.pose.pose.position.z = 0.0;
+
+
+		// Set the velocity
+		odom.child_frame_id = "base_link";
+		odom.twist.twist=e2_twist.twist;
+
+		// Publish the message
+		odom_pub.publish(odom);
+
+		last_time = current_time;
+
+		usleep(RATE);
+
+	}
+	ROS_INFO("Fake odom ended......");
+}
