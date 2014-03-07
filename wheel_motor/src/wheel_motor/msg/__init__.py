2a6ed6d7bb76192d23b0baa4aceeb09a4cc2508a
boottp <sr.jackal@gmail.com>
2014-03-07 12:55:07 +0100

    package deleted because no more usefull

--- wheel_motor/src/wheel_motor/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._WheelFeedback import *
-from ._WheelActionFeedback import *
-from ._WheelGoal import *
-from ._WheelAction import *
-from ._WheelActionResult import *
-from ._WheelResult import *
-from ._WheelActionGoal import *
