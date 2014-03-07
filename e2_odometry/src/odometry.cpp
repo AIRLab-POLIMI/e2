cc803c23dba42e7d1c01f2c1641e7efa3da903fd
boottp <sr.jackal@gmail.com>
2014-03-07 15:17:52 +0100

    Odometry package

---
+++ e2_odometry/src/odometry.cpp
@@ -0,0 +1,109 @@
+/*
+ * odometry.cpp - AIRLab (Politecnico di Milano)
+ * 
+ *  Author:  Lorenzo Ripani 
+ *  Email: ripani.lorenzo@gmail.com
+ *
+ *  Created on: 15/feb/2014
+ *
+ */
+
+#include "odometry.h"
+
+//==================================================================
+//		Constructor
+//==================================================================
+Odometry::Odometry()
+{
+	// Initialize Odometry position
+	x = 0.0;
+	y = 0.0;
+	th = 0.0;
+
+	odom_quat.x=0;
+	odom_quat.y=0;
+	odom_quat.z=0;
+	odom_quat.w=1;
+
+	// Initial time
+	last_time=ros::Time::now();
+	current_time=ros::Time::now();
+};
+
+//==================================================================
+//		Constructor to set initial robot pose
+//==================================================================
+Odometry::Odometry(geometry_msgs::Pose pose)
+{
+	// Initialize Odometry position
+	x = pose.position.x;
+	y = pose.position.y;
+
+	odom_quat = pose.orientation;
+	th = tf::getYaw(pose.orientation);
+
+	// Initial time
+	last_time=ros::Time::now();
+	current_time=ros::Time::now();
+};
+//==================================================================
+//		Distructor
+//==================================================================
+
+Odometry::~Odometry()
+{
+	x = 0.0;
+	y = 0.0;
+	th = 0.0;
+
+	vx = 0.0;
+	vr = 0.0;
+};
+
+//==================================================================
+//		Calculate odometry data based on robot velocity - TODO use encoder data
+//==================================================================
+void Odometry::ComputeOdometry(float d_left, float d_right)
+{
+
+	current_time=ros::Time::now();
+	double elapsed = (current_time - last_time).toSec();
+	last_time=current_time;
+
+	// Distance travelled
+	double d = (d_left + d_right) / 2;
+	double d_th = (d_right - d_left) / ROBOT_WIDTH;
+
+	// Set velocity from calculated data
+	vx = d / elapsed ;
+	vr =d_th / elapsed ;
+	double d_x,d_y;
+
+	if( d != 0 )
+	{
+		d_x = cos(d_th) * d ;
+		d_y = -sin(d_th) * d ;
+
+		x = x + (cos(th) * d_x - sin(th) * d_y ) ;
+		y = y + (sin(th) * d_x + cos(th) * d_y ) ;
+	}
+	if(d_th != 0)
+		th = th + d_th ;
+
+	odom_quat.x = 0;
+	odom_quat.y = 0;
+	odom_quat.z = sin(th/2);
+	odom_quat.w = cos(th/2);
+
+	ROS_DEBUG("-----------------------------------------------------------------------");
+	ROS_DEBUG("[Odom]:: ----- Time passed from last calculation %f",elapsed);
+	ROS_DEBUG("[Odom]:: ----- Distance Travelled right %f",d_right);
+	ROS_DEBUG("[Odom]:: ----- Distance Travelled left %f",d_left);
+	ROS_DEBUG("[Odom]:: ----- Distance Travelled %f",d);
+	ROS_DEBUG("[Odom]:: ----- Linear Velocity %f m/s",vx);
+	ROS_DEBUG("[Odom]:: ----- Angular Velocity %f rad/s",vr);
+	ROS_DEBUG("[Odom]:: ----- Delta X %f",d_x);
+	ROS_DEBUG("[Odom]:: ----- Delta Y %f",d_y);
+	ROS_DEBUG("[Odom]:: ----- Delta TH %f",d_th);
+	ROS_DEBUG("-----------------------------------------------------------------------");
+};
---
+++ e2_odometry/src/odometry.h
@@ -0,0 +1,53 @@
+/*
+ * odometry.h - AIRLab (Politecnico di Milano)
+ * 
+ *  Author:  Lorenzo Ripani 
+ *  Email: ripani.lorenzo@gmail.com
+ *
+ *  Created on: 15/feb/2014
+ *
+ */
+
+#ifndef ODOMETRY_H_
+#define ODOMETRY_H_
+
+#include "nav_msgs/Odometry.h"
+#include <geometry_msgs/Quaternion.h>
+#include <geometry_msgs/Pose.h>
+#include <tf/transform_broadcaster.h>
+
+#define ROBOT_WIDTH 0.6
+
+class Odometry
+{
+	private:
+
+		float d_left,d_right;
+		tf::TransformBroadcaster odom_broadcaster;
+
+	public:
+
+		double x ;
+		double y ;
+		double th ;
+
+		double vx;
+		double vr ;
+
+		geometry_msgs::Quaternion odom_quat;
+		ros::Time current_time, last_time;
+
+		// Constructor
+		Odometry();
+		Odometry(geometry_msgs::Pose pose);
+		~Odometry();
+
+		/*
+		 * Compute odometry based on robot speeds
+		 *  @param tanSpeed	linear speed of robot
+		 *  @param rotSpeed rotational speed
+		 */
+		void ComputeOdometry(float d_left,float d_right);
+};
+
+#endif /* ODOMETRY_H_ */
