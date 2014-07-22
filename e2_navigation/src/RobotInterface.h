32f3a9eaf77e2f54e58f8df8312d31bf2dff8e8a
Lorenzo Ripani <sr.jackal@gmail.com>
2014-07-22 15:59:23 +0200

    minor

--- e2_navigation/src/RobotInterface.h
+++ e2_navigation/src/RobotInterface.h
@@ -11,7 +11,7 @@
 #ifndef ROBOTINTERFACE_H_
 #define ROBOTINTERFACE_H_
 
-#define BASE_ROTATION_ANGLE 	0.785					// Radians 45°
+#define BASE_ROTATION_ANGLE 	3.14					//  180 gradi
 #define SPEECH_DELAY			30						// Time between each speech
 #define ERROR_DISTANCE			8						// distance greater than this will be considered as kinect error and so discarded. Mainly used for simulation bug
 
